1aecf9b80472adf4059ba575bf3c48fca351c25b
Randriamahefa <974pascalito974@gmail.com>
2023-05-14 21:58:17 +0400

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 const Footer = () => {
-    return (
-        <footer className="bg-body-secondary text-center text-lg-start" data-bs-theme="dark">
+  return (
+    <footer className="bg-body-secondary text-center text-lg-start" data-bs-theme="dark">
       <div className="container p-4">
         <div className="row">
-          <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+          <div className="col-lg-4 col-md-6 mb-4 mb-md-0">
             <h5 className="text-uppercase">À propos de nous</h5>
             <p>
-                34, rue de la Clinique, Commerce n°5,
+              34, rue de la Clinique, Commerce n°5,
             </p>
             <p>
-                Rés.les Colonies, BP167, 97492 Ste Clotilde Cédex
+              Rés.les Colonies, BP167, 97492 Ste Clotilde Cédex
             </p>
           </div>
-          <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <div className="col-lg-3 col-md-3 mb-4 mb-md-0">
             <h5 className="text-uppercase">Liens utiles</h5>
             <ul className="list-unstyled mb-0">
               <li>
@@ -33,21 +33,24 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="social-media">
+          <div className="rowSocial">
             <h5 className="text-uppercase mb-0">Suivez-nous</h5>
-                <a href="#!" className="facebook"><img id="facebook" src='../facebook.png' /></a>
-                <a href="#!" className="youtube"><img id="youtube" src='../youtube.png' /></a>
-                <a href="#!" className="twitter"><img id="twitter" src='../twitter.png' /></a>
-                <a href="#!" className="insta"><img id="insta" src='../insta.png' /></a>
-                <a href="#!" className="tiktok"><img id="tiktok" src='../tiktok.png' /></a>
+            <div className="social-media">
+
+              <a href="#!" className="facebook"><img id="facebook" src='../facebook.png' /></a>
+              <a href="#!" className="youtube"><img id="youtube" src='../youtube.png' /></a>
+              <a href="#!" className="twitter"><img id="twitter" src='../twitter.png' /></a>
+              <a href="#!" className="insta"><img id="insta" src='../insta.png' /></a>
+              <a href="#!" className="tiktok"><img id="tiktok" src='../tiktok.png' /></a>
+            </div>
           </div>
         </div>
       </div>
       <div className="text-center p-3 bg-body-tertiary">
-        © 2023 Video Drive - Tous droits réservés
+        © 2023 VideoDrive - Tous droits réservés
       </div>
     </footer>
-    );
+  );
 };
 
 export default Footer;
