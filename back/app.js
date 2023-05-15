3ad9a50cb6ffaa652646137095a9f8d4ace3cbd9
Randriamahefa <974pascalito974@gmail.com>
2023-05-15 13:25:23 +0400

    integration

--- back/app.js
+++ back/app.js
@@ -11,6 +11,12 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://192.168.0.47:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use(express.json());
 
 app.get('/', (req, res) => {
