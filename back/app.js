d83a6cd64d2c0d6883e2e23e925ec39d91e4d4b1
Randriamahefa <974pascalito974@gmail.com>
2023-05-15 15:31:04 +0400

    carousel

--- back/app.js
+++ back/app.js
@@ -3,6 +3,7 @@ const app = express();
 const port = 3000;
 const userRoutes = require('./routes/users');
 const produitRoutes = require('./routes/produits');
+
 const db = require('./models');
 const stripe = require('stripe')('sk_test_51N7udGD8fIYjfM4wWy0pXFQ1zQYLRJlNa1a1w1fAiTxmWkIUVrdzdyN9vKdEagyjhZafnoP50nmGTccDJvRJYEip00BZQltPq3');
 
@@ -22,7 +23,23 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoutes);
 app.use('/produits', produitRoutes); // Utilisation de 'produits' au lieu de 'produit'
+app.post('/paiement', async (req, res) => {
+    try {
+        // Récupérer le prix à partir de la base de données
+        const prix = 10;
 
+        // Créer le paiement avec le prix de la base de données
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount: prix * 100, // Convertir le prix en centimes
+            currency: 'eur',
+            payment_method_types: ['card'],
+        });
+
+        res.json({ clientSecret: paymentIntent.client_secret });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
 db.sequelize.sync().then(() => {
     app.listen(port, () => {
         console.log(`App listening at http://localhost:${port}`);
